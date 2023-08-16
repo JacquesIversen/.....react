ac0d349df19bae375fcd56bc36a9a6a04b8d46ba
JacquesIversen <jackiracki1@gmail.com>
2023-08-16 13:55:57 +0000

    added components

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/* function FunctionalGreeting () {
+    return <h1>Hello from FunctionalGreeting</h1>
+} */
+
+const FunctionalGreeting = () => <h1>Hello from Arrow Function</h1>
+
+
+export default FunctionalGreeting
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/* function FunctionalGreeting () {
+    return <h1>Hello from FunctionalGreeting</h1>
+} */
+
+const FunctionalGreetingWithProps = () => <h1>Hello from Prop Function</h1>
+
+
+export default FunctionalGreetingWithProps
